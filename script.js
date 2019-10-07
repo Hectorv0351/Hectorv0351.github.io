ae171e87fa55cc3aa59de3ff2e1d1dcb01af901a
Hectorv0351 <56263440+Hectorv0351@users.noreply.github.com>
2019-10-07 10:23:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Hector Vargas"
+let userName = "hectorv0351";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
